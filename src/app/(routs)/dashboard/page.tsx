86f21b51d8d874cf8faf12f797f402570f7fa82a
Gustavo Rabelo Frere <148499706+rbll0@users.noreply.github.com>
2024-06-06 17:29:43 -0300

    feat: implement authentication logic and display personalized greeting on dashboard page

--- src/app/(routs)/dashboard/page.tsx
+++ src/app/(routs)/dashboard/page.tsx
@@ -1,5 +1,18 @@
+"use client"
+
+import React from "react";
+
 export default function Dashboard() {
+    const token = sessionStorage.getItem("userToken");
+    const fullname = sessionStorage.getItem("fullname");
+
     return (
-        <h1>Hello World!</h1>
-    )
+        <React.Fragment>
+            {token ? (
+                <h1>Hello, {fullname}!</h1>
+            ) : (
+                <h1>Você não está autorizado a acessar esta página, por favor faça login e tente novamente.</h1>
+            )}
+        </React.Fragment>
+    );
 }
